4920bca1f9b78697506553b478720999188bcf51
cuicanrensheng <1693728915@qq.com>
2026-05-14 18:11:19 +0800

    Update Constant.kt

--- app/src/main/java/com/tv/live/Constant.kt
+++ app/src/main/java/com/tv/live/Constant.kt
@@ -1,12 +1,12 @@
-package com.tv.live
+package com.tv.live;
 
-object Constant {
+public class Constant {
     // 默认直播源
-    const val DEFAULT_M3U = "https://gitee.com/qf_1111/iptv/raw/master/playlist.m3u"
+    public static final String DEFAULT_M3U = "https://gitee.com/qf_1111/iptv/raw/master/playlist.m3u";
     // 网页设置端口 10481
-    const val WEB_SET_PORT = 10481
+    public static final int WEB_SET_PORT = 10481;
     // 缓存时间
-    const val CACHE_DURATION = 30
+    public static final int CACHE_DURATION = 30;
     // 强制IPv6
-    const val ONLY_IPV6 = true
+    public static final boolean ONLY_IPV6 = true;
 }

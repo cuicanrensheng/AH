3d42e336b26d35947f82ce7b7bc014743cc907eb
cuicanrensheng <1693728915@qq.com>
2026-05-15 14:14:29 +0800

    Update Channel.java

--- app/src/main/java/com/tv/live/model/Channel.java
+++ app/src/main/java/com/tv/live/model/Channel.java
@@ -1,24 +1,20 @@
 package com.tv.live.model;
-
 public class Channel {
-    private long id;
+    private int id;
     private String name;
     private String url;
     private boolean isFavorite;
 
-    public Channel(long id, String name, String url) {
-        this(id, name, url, false);
-    }
-
-    public Channel(long id, String name, String url, boolean isFavorite) {
+    public Channel(int id, String name, String url, boolean isFavorite) {
         this.id = id;
         this.name = name;
         this.url = url;
         this.isFavorite = isFavorite;
     }
 
-    public long getId() { return id; }
+    public int getId() { return id; }
     public String getName() { return name; }
     public String getUrl() { return url; }
     public boolean isFavorite() { return isFavorite; }
+    public void setFavorite(boolean favorite) { isFavorite = favorite; }
 }

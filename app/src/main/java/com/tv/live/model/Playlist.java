6edde7de10356c1b1510291f70664487383d0ac6
cuicanrensheng <1693728915@qq.com>
2026-05-15 11:57:10 +0800

    Create Playlist.java

---
+++ app/src/main/java/com/tv/live/model/Playlist.java
@@ -0,0 +1,26 @@
+package com.tv.live.model;
+
+import java.util.List;
+
+public class Playlist {
+    private long id;
+    private String name;
+    private String url;
+    private List<Channel> channels;
+
+    public Playlist(long id, String name, String url) {
+        this(id, name, url, new ArrayList<>());
+    }
+
+    public Playlist(long id, String name, String url, List<Channel> channels) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+        this.channels = channels;
+    }
+
+    public long getId() { return id; }
+    public String getName() { return name; }
+    public String getUrl() { return url; }
+    public List<Channel> getChannels() { return channels; }
+}

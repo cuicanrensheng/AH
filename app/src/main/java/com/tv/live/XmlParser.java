472115e340e2bfb7995d5340e29c5e0aad7229a1
cuicanrensheng <1693728915@qq.com>
2026-05-27 12:41:16 +0800

    Update XmlParser.java

--- app/src/main/java/com/tv/live/XmlParser.java
+++ app/src/main/java/com/tv/live/XmlParser.java
@@ -4,6 +4,7 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -16,8 +17,9 @@ public class XmlParser {
             xml.setInput(is, StandardCharsets.UTF_8.name());
 
             Calendar today = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
             String currentChannel = null;
-            List<Channel.EpgItem> items = new ArrayList<>();
+            List<MainActivity.Channel.EpgItem> items = new ArrayList<>();
 
             while (xml.getEventType() != XmlPullParser.END_DOCUMENT) {
                 String tag = xml.getName();
@@ -26,35 +28,30 @@ public class XmlParser {
                         currentChannel = xml.getAttributeValue(null, "id");
                     }
                     if ("display-name".equals(tag)) {
-                        String name = xml.nextText().trim();
-                        currentChannel = name;
+                        currentChannel = xml.nextText().trim();
                     }
                     if ("programme".equals(tag)) {
                         String start = xml.getAttributeValue(null, "start");
                         String stop = xml.getAttributeValue(null, "stop");
-                        String channel = xml.getAttributeValue(null, "channel");
+                        String channelId = xml.getAttributeValue(null, "channel");
 
                         Calendar sCal = Calendar.getInstance();
-                        try {
-                            sCal.setTime(new SimpleDateFormat("yyyyMMddHHmmss").parse(start));
-                        } catch (Exception e) {}
+                        sCal.setTime(sdf.parse(start));
+                        String dayName = EpgManager.getInstance().getDayName(sCal, today);
+                        String time = start.substring(8, 10) + ":" + start.substring(10, 12)
+                                + " - " + stop.substring(8, 10) + ":" + stop.substring(10, 12);
+                        String playUrl = "http://epg.51zmt.top:8000/" + channelId + "/"
+                                + start.substring(0, 8) + "/" + start.substring(8, 14) + ".m3u8";
 
-                        String dayName = EpgManager.getDayText(sCal, today);
-                        String time = start.substring(8,10)+":"+start.substring(10,12)
-                                + " - " + stop.substring(8,10)+":"+stop.substring(10,12);
-                        String title = "";
-                        String playUrl = "http://epg.51zmt.top:8000/"+channel+"/"+start.substring(0,8)+"/"+start.substring(8,14)+".m3u8";
-
-                        Channel.EpgItem item = new Channel.EpgItem();
+                        MainActivity.Channel.EpgItem item = new MainActivity.Channel.EpgItem();
                         item.dayName = dayName;
                         item.time = time;
-                        item.title = title;
                         item.playUrl = playUrl;
                         items.add(item);
                     }
                     if ("title".equals(tag)) {
                         if (!items.isEmpty()) {
-                            items.get(items.size()-1).title = xml.nextText().trim();
+                            items.get(items.size() - 1).title = xml.nextText().trim();
                         }
                     }
                 }
@@ -67,6 +64,6 @@ public class XmlParser {
     }
 
     public interface EpgCallback {
-    void onParsed(String channelName, List<MainActivity.Channel.EpgItem> items);
+        void onParsed(String channelName, List<MainActivity.Channel.EpgItem> items);
     }
 }

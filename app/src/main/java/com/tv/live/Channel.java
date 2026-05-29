e02f467fe3fd2da150b420b0f6c0cc9bf949665c
cuicanrensheng <1693728915@qq.com>
2026-05-29 14:47:48 +0800

    Update Channel.java

--- app/src/main/java/com/tv/live/Channel.java
+++ app/src/main/java/com/tv/live/Channel.java
@@ -17,35 +17,18 @@ public class Channel {
         return playUrl;
     }
 
-    // ↓↓↓ 就是缺了这个内部类，导致全部报红 ↓↓↓
     public static class EpgItem {
-        private String day;
-        private String time;
-        private String title;
-        private boolean isReplay;
-        private String replayUrl;
+        public String day;
+        public String time;
+        public String title;
+        public boolean isReplay;
+        public String replayUrl;
 
         public EpgItem(String day, String time, String title, boolean isReplay) {
             this.day = day;
             this.time = time;
             this.title = title;
             this.isReplay = isReplay;
-        }
-
-        public String getDay() {
-            return day;
-        }
-
-        public String getTime() {
-            return time;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public boolean isReplay() {
-            return isReplay;
         }
 
         public String getReplayUrl() {

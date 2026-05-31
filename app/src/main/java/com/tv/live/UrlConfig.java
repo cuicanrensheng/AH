f32dbc0400899b54f060da8e0053e61367c2e12c
cuicanrensheng <1693728915@qq.com>
2026-05-31 09:28:51 +0800

    Update UrlConfig.java

--- app/src/main/java/com/tv/live/UrlConfig.java
+++ app/src/main/java/com/tv/live/UrlConfig.java
@@ -1,15 +1,5 @@
 package com.tv.live;
-
-/**
- * 直播源 + EPG 统一配置文件
- * 支持 .gz 自动解压
- */
 public class UrlConfig {
-
-    //===================== 直播源链接 =====================
-    public static String LIVE_URL = "https://gitee.com/qf_1111/iptv/raw/master/playlist.m3u";
-
-    //===================== EPG 链接（支持 .gz 解压） =====================
-    public static String EPG_URL = "https://e.erw.cc/all.xml.gz";
-
+    public static final String LIVE_URL = "https://gitee.com/qf_1111/iptv/raw/master/playlist.m3u";
+    public static final String EPG_URL = "https://epg.catvod.com/epg.xml";
 }

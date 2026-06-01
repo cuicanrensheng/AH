8fdd9bf1b80bd0d34501b084ffa4a20dbc339b26
cuicanrensheng <1693728915@qq.com>
2026-06-01 08:54:56 +0800

    Update MainActivity.java

--- app/src/main/java/com/tv/live/MainActivity.java
+++ app/src/main/java/com/tv/live/MainActivity.java
@@ -1,8 +1,8 @@
 package com.tv.live;
-import com.tv.live.ChannelListManager;
-import com.tv.live.GroupListManager;
-import com.tv.live.DateListManager;
-import com.tv.live.EpgManagerWrapper;
+import com.tv.live.widget.ChannelListManager;
+import com.tv.live.widget.GroupListManager;
+import com.tv.live.widget.DateListManager;
+import com.tv.live.widget.EpgManagerWrapper;
 
 import com.tv.live.SettingsActivity;
 import android.content.BroadcastReceiver;

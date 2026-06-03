06669147efdb21b0b1596acc9df25cd8eac05b49
cuicanrensheng <1693728915@qq.com>
2026-06-03 09:25:21 +0800

    Update UrlConfig.java

--- app/src/main/java/com/tv/live/UrlConfig.java
+++ app/src/main/java/com/tv/live/UrlConfig.java
@@ -1,7 +1,10 @@
 package com.tv.live;
-
 public class UrlConfig {
-    // 去掉 final，允许 MainActivity 动态修改
-    public static String LIVE_URL = "https://gitee.com/qf_1111/iptv/raw/master/playlist.m3u";
-    public static String EPG_URL = "https://epg.catvod.com/epg.xml";
+    //默认源常量（修复报错缺少字段）
+    public static final String DEFAULT_LIVE = "https://gitee.com/qf_1111/iptv/raw/master/playlist.m3u";
+    public static final String DEFAULT_EPG = "https://epg.catvod.com/epg.xml";
+
+    //运行时可修改地址
+    public static String LIVE_URL = DEFAULT_LIVE;
+    public static String EPG_URL = DEFAULT_EPG;
 }

864c537985f2bf53801917c32f9e38986e782ae2
cuicanrensheng <1693728915@qq.com>
2026-06-04 20:35:44 +0800

    Update KeyEventManager.java

--- app/src/main/java/com/tv/live/manager /KeyEventManager.java
+++ app/src/main/java/com/tv/live/manager /KeyEventManager.java
@@ -11,28 +11,24 @@ public class KeyEventManager {
         this.activity = activity;
     }
 
-    public boolean dispatchKeyEvent(KeyEvent event) {
-        int key = event.getKeyCode();
+    public boolean dispatchKey(int keyCode) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_UP:
+                activity.playPrev();
+                return true;
 
-        if (event.getAction() == KeyEvent.ACTION_UP) {
-            switch (key) {
-                case KeyEvent.KEYCODE_DPAD_CENTER:
-                    activity.togglePanel();
-                    return true;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                activity.playNext();
+                return true;
 
-                case KeyEvent.KEYCODE_MENU:
-                case KeyEvent.KEYCODE_HELP:
-                    activity.openSettings();
-                    return true;
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+            case KeyEvent.KEYCODE_ENTER:
+                activity.togglePanel();
+                return true;
 
-                case KeyEvent.KEYCODE_DPAD_UP:
-                    activity.playPrev();
-                    return true;
-
-                case KeyEvent.KEYCODE_DPAD_DOWN:
-                    activity.playNext();
-                    return true;
-            }
+            case KeyEvent.KEYCODE_MENU:
+                activity.openSettings();
+                return true;
         }
         return false;
     }

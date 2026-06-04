c16e83864ba0eae47c49d65829240a1f3e4fb8f5
cuicanrensheng <1693728915@qq.com>
2026-06-04 12:17:38 +0800

    Update DateListManager.java

--- app/src/main/java/com/tv/live/widget/DateListManager.java
+++ app/src/main/java/com/tv/live/widget/DateListManager.java
@@ -4,11 +4,11 @@ import android.content.Context;
 import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.tv.live.MainActivity;
+import com.tv.live.R;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -17,21 +17,19 @@ public class DateListManager {
     private final ListView lvDate;
     private final Context context;
     private int selectedPosition = 0;
+    private OnDateSelectedListener listener;
+
+    public interface OnDateSelectedListener {
+        void onDateSelected(int position);
+    }
+
+    public void setOnDateSelectedListener(OnDateSelectedListener listener) {
+        this.listener = listener;
+    }
 
     public DateListManager(Context context, ListView lvDate) {
         this.context = context;
         this.lvDate = lvDate;
-        lvDate.setItemsCanFocus(true);
-
-        lvDate.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                selectedPosition = pos;
-                ((ArrayAdapter<?>) parent.getAdapter()).notifyDataSetChanged();
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
     }
 
     public void initDate() {
@@ -41,31 +39,31 @@ public class DateListManager {
 
         for (int i = 0; i < 8; i++) {
             int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-            if (i == 0) {
-                dates.add("今天");
-            } else {
-                dates.add(week[dayOfWeek % 7]);
-            }
+            String display = i == 0 ? "今天" : week[dayOfWeek % 7];
+            dates.add(display);
             cal.add(Calendar.DAY_OF_YEAR, 1);
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(lvDate.getContext(), android.R.layout.simple_list_item_1, dates) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, R.layout.item_date, dates) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-                TextView tv = view.findViewById(android.R.id.text1);
-                tv.setTextSize(14);
+                TextView tv = (TextView) super.getView(position, convertView, parent);
                 tv.setTextColor(position == selectedPosition ? Color.parseColor("#40A9FF") : Color.WHITE);
-                return view;
+                return tv;
             }
         };
+
         lvDate.setAdapter(adapter);
+        lvDate.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPosition = position;
+            adapter.notifyDataSetChanged();
+            if (listener != null) {
+                listener.onDateSelected(position);
+            }
+        });
     }
 
     public void setSelectedPosition(int position) {
         selectedPosition = position;
-        lvDate.setSelection(position);
     }
-
-    public void onBackPressed() {}
 }

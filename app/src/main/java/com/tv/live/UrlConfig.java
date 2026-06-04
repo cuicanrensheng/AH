29c244a720dcc6317528415a42d48ad527148bb8
cuicanrensheng <1693728915@qq.com>
2026-06-04 22:06:19 +0800

    Add files via upload

---
+++ app/src/main/java/com/tv/live/UrlConfig.java
@@ -0,0 +1,7 @@
+package com.tv.live;
+
+public class UrlConfig {
+    // 去掉 final，允许 MainActivity 动态修改
+    public static String LIVE_URL = "https://gitee.com/qf_1111/iptv/raw/master/playlist.m3u";
+    public static String EPG_URL = "https://epg.catvod.com/epg.xml";
+}

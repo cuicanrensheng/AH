b2e4b534111633aac2c032d72b973b21322f791c
cuicanrensheng <1693728915@qq.com>
2026-06-06 14:46:37 +0800

    Create WidgetInitManager.java

---
+++ app/src/main/java/com/tv/live/manager/WidgetInitManager.java
@@ -0,0 +1,34 @@
+package com.tv.live.manager;
+
+import android.content.Context;
+import android.widget.ListView;
+import com.tv.live.widget.ChannelListManager;
+import com.tv.live.widget.DateListManager;
+import com.tv.live.widget.EpgManagerWrapper;
+import com.tv.live.widget.GroupListManager;
+
+public class WidgetInitManager {
+    private final Context context;
+
+    public WidgetInitManager(Context context) {
+        this.context = context;
+    }
+
+    public ChannelListManager createChannelListManager(ListView lv) {
+        return new ChannelListManager(context, lv);
+    }
+
+    public GroupListManager createGroupListManager(ListView lv) {
+        return new GroupListManager(context, lv);
+    }
+
+    public EpgManagerWrapper createEpgManagerWrapper(ListView lv) {
+        return new EpgManagerWrapper(context, lv);
+    }
+
+    public DateListManager createDateListManager(ListView lv) {
+        DateListManager manager = new DateListManager(context, lv);
+        manager.initDate();
+        return manager;
+    }
+}

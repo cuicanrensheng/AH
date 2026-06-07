37b49699f54cd4fba38a59f8ba022d4ad0a9b51d
cuicanrensheng <1693728915@qq.com>
2026-06-07 13:52:07 +0800

    Delete app/src/main directory

--- app/src/main/java/com/tv/live/UrlConfig.java
+++
@@ -1,5 +0,0 @@
-package com.tv.live;
-public class UrlConfig {
-    public static  String LIVE_URL = "https://raw.githubusercontent.com/cuicanrensheng/IPTV/refs/heads/main/playlist1.m3u";
-    public static  String EPG_URL = "https://epg.catvod.com/epg.xml";
-}

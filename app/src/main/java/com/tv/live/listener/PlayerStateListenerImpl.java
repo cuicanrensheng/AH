0a24424611786d440c881609f8a0762c026b744d
cuicanrensheng <1693728915@qq.com>
2026-06-17 13:46:16 +0800

    Update PlayerStateListenerImpl.java

--- app/src/main/java/com/tv/live/listener/PlayerStateListenerImpl.java
+++ app/src/main/java/com/tv/live/listener/PlayerStateListenerImpl.java
@@ -20,7 +20,44 @@ public class PlayerStateListenerImpl implements TVPlayerManager.OnPlayStateListe
     public void onIdle() {}
 
     @Override
-    public void onBuffering() {
+    public void onBuffering() package com.tv.live.listener;
+
+import android.content.Context;
+import android.widget.Toast;
+import com.tv.live.TVPlayerManager;
+
+public class PlayerStateListenerImpl implements TVPlayerManager.OnPlayStateListener {
+    private final Context context;
+    private String currentChannelName = "";
+
+    public PlayerStateListenerImpl(Context context) {
+        this.context = context.getApplicationContext();
+    }
+
+    public void setCurrentChannelName(String name) {
+        this.currentChannelName = name;
+    }
+
+    @Override
+    public void onIdle() {}
+
+    @Override
+    public void onBuffering() {}
+
+
+    @Override
+    public void onPlayReady() {}
+
+    @Override
+    public void onPlayEnd() {
+        Toast.makeText(context, "播放结束，自动重试", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPlayError(String msg) {
+        Toast.makeText(context, "播放异常：" + msg, Toast.LENGTH_SHORT).show();
+    }
+}
 
 
     @Override

5ba36fe8056c83ed2e6cd105ca407fc0bd01e99c
cuicanrensheng <1693728915@qq.com>
2026-08-02 20:57:12 +0800

    Delete app directory

--- app/src/main/java/com/tv/live/UrlConfig.java
+++
@@ -1,6 +0,0 @@
-package com.tv.live;
-
-public class UrlConfig {
-    public static String LIVE_URL = "https://gitee.com/qf_1111/iptv/raw/master/playlist.m3u";
-    public static String EPG_URL = "https://e.erw.cc/all.xml.gz";
-}

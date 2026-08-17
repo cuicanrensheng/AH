626369df6e5b33d7349c870ad13871009c0e0213
cuicanrensheng <1693728915@qq.com>
2026-08-17 21:02:47 +0800

    Delete app/src/main/assets/js directory

--- app/src/main/assets/js/yt_source_root.js
+++
@@ -1,6 +0,0 @@
-function getParser(url) {
-    if (url.indexOf('huya.com') !== -1 || url.indexOf('huya://') !== -1) {
-        return JSON.stringify(['huya_parse.js', 'parseHuya']);
-    }
-    return JSON.stringify([]);
-}
